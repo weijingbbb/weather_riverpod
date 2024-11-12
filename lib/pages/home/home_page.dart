8a2eac0fe7347b961cb6ad24bce49024be99a4b9
weijingbbb <1047129443@qq.com>
2024-11-12 23:28:03 +0800

    添加主页搜索逻辑和UI

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_riverpod/extensions/async_value_xx.dart';
+import 'package:weather_riverpod/models/current_weather/current_weather.dart';
+import 'package:weather_riverpod/models/custom_error/custom_error.dart';
 import 'package:weather_riverpod/pages/home/providers/weather_provider.dart';
+import 'package:weather_riverpod/pages/home/widgets/show_weather.dart';
 import 'package:weather_riverpod/pages/search/search_page.dart';
 
 class HomePage extends ConsumerStatefulWidget {
@@ -14,16 +17,34 @@ class HomePage extends ConsumerStatefulWidget {
 class _HomePageState extends ConsumerState<HomePage> {
   String? city;
 
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 1), () {
-      ref.read(weatherProvider.notifier).fetchWeather('London');
-    });
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Future.delayed(const Duration(seconds: 1), () {
+  //     ref.read(weatherProvider.notifier).fetchWeather('London');
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
+    ref.listen<AsyncValue<CurrentWeather?>>(
+      weatherProvider,
+      (previous, next) {
+        next.whenOrNull(
+          error: (error, stackTrace) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text((error as CustomError).errMsg),
+                );
+              },
+            );
+          },
+        );
+      },
+    );
+
     final weatherState = ref.watch(weatherProvider);
     print('homepage----${weatherState.toStr}');
 
@@ -47,8 +68,14 @@ class _HomePageState extends ConsumerState<HomePage> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: ShowWeather(weatherState: weatherState),
+      floatingActionButton: FloatingActionButton(
+        onPressed: city == null
+            ? null
+            : () {
+                ref.read(weatherProvider.notifier).fetchWeather(city!);
+              },
+        child: const Icon(Icons.refresh),
       ),
     );
   }
---
+++ lib/pages/home/widgets/select_city.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SelectCity extends StatelessWidget {
+  const SelectCity({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Select a city',
+        style: TextStyle(fontSize: 20.0),
+      ),
+    );
+  }
+}
